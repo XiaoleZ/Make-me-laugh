9d58cf0dedcfd22e1c511801d6a89c4a99256506
soomyy <omaarasma20@gmail.com>
2019-01-19 20:17:25 -0700

    added a function to generate a random joke

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+
+var min = 0;
+var j, joke, num;
+var n = 2;
+var max = n-1;
+//new jokes(n);			//Array of size n 
+
+var joke_list = ['hello','world']
+
 document.getElementById('joke_button').addEventListener('click', function(){
-	// document.getElementById("demo").innerHTML = "Hello World";
+	generateJoke();
 });
+
+
+
+function getRandomInt(min, max) {		//returns a random integer
+   	min = Math.ceil(min);
+    	max = Math.floor(max);
+    	num = Math.floor(Math.random() * (max - min + 1)) + min;
+	console.log(num);
+	return num;
+}
+
+
+function generateJoke() {			//generates a random joke 
+	j = getRandomInt(min, max);
+	console.log(j);
+
+	joke = joke_list[j];
+	console.log(joke);
+	return joke;
+}
+
+
+
+
+
+
